586a336e63839aa9f813c9c6d34b8e2c17d88591
avital <avitalz57@gmail.com>
2020-06-18 19:27:44 +0300

    login

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -43,7 +43,7 @@ export default {
       if (type && type == "random" && this.recipeType == type) {
         try {
           const response = await this.axios.get(
-            "https://test-for-3-2.herokuapp.com/recipes/random"
+            "https://assignment-3-2-avital.herokuapp.com/recipe/random"
           );
 
           // console.log(response);
